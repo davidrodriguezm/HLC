3ab8ecddead822904e83ac40c7d2afe4f77cbdd2
davidrodriguezm <alum.drodriguezm@iesalixar.org>
2020-12-15 04:16:09 +0100

    cambio en ejercicio

--- prueba_py/ejercicio_2.py
+++ prueba_py/ejercicio_2.py
@@ -1,16 +1,19 @@
 # Se intenta adivinar una contraseña con cada intento fallido se descubre un carácter
 
 contra="loquesea"
+respuesta=""
 cont=0
-while True:
+while respuesta != contra:
     respuesta = input("introduce la contraseña. Si queres terminar responde n o N: ")
     cont+=1
-    if respuesta.upper()=="N" or respuesta==contra:
+    if respuesta.upper()=="N":
         break
     elif not(respuesta.upper()=="N" or respuesta==contra) and cont<len(contra):
         print("La contraseña empiezapor: ",contra[0:cont])
     elif not(respuesta.upper()=="N" or respuesta==contra) and cont>=len(contra):
         print("La contraseña empiezapor: ",contra)
+else:
+    print("Correcto has completado el juego")
 
 if respuesta==contra and cont<len(contra):
     print("Lo has acertado al intento",cont)
