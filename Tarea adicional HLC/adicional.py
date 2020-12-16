a0a79ed982ebb9ea025d0f1cbf2a95d74d002246
davidrodriguezm <alum.drodriguezm@iesalixar.org>
2020-12-17 00:27:47 +0100

    Update adicional.py

--- Tarea adicional HLC/adicional.py
+++ Tarea adicional HLC/adicional.py
@@ -7,13 +7,28 @@ def filtrar_status( dic ):
     else :
         return False
 
+def devolver_mapas( dic ): 
+    resul = {}
+    resul["name"] = dic.get("name")
+    resul["cpu"] = dic.get("cpu")
+    resul["mem"] = dic.get("mem")
+    return resul  
+
 def procesar_json():
     resul = None
-
     with open("salida-json.txt") as fichero:
         datos = json.load( fichero )
-        resul = list(filter( filtrar_status, datos ))
-
+        filtrado = list(filter( filtrar_status, datos ))
+        resul = list(map(devolver_mapas, filtrado))
+        resul.sort( key=lambda elem: elem["cpu"], reverse=True)   
     return resul
 
-print( procesar_json() )
+lista = procesar_json()
+suma = 0
+
+print("Diccionarios ordenados por orden descendente de cpu:")
+for i in lista:
+    suma += i.get("mem")
+    print(i)
+else:
+    print("\nLa suma de todos los men:",suma)
