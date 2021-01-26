9e02cd1b19ab6f04f8af1e550e7a32e11b2b5156
davidrodriguezm <alum.drodriguezm@iesalixar.org>
2021-01-26 18:23:04 +0100

    ejercicio 11 mejorado

--- prueba_py/ejercicio_11.py
+++ prueba_py/ejercicio_11.py
@@ -6,23 +6,41 @@ def operaciones(nombre):
             fecha = datetime.now()
             print('hoy es dia {} del mes {} del año {}'.format( fecha.day, fecha.month, fecha.year ) )
             print('Se ha llamado a:', nombre)
-            return funcion(*args, **kwargs)
+            return funcion( *args, **kwargs)
 
         return envoltorio
 
     return decorador
 
 @operaciones('suma')
-def suma( a, b, c = 0, d = 0 ):
-    return a + b + c + d
+def suma( *args, **kwargs ):
+    resul = 0
+    if  args : 
+        for i in args:
+            resul += i
+
+    if  kwargs : 
+        for v in kwargs.values():
+            resul += v  
+
+    return resul
 
 @operaciones('multiplicar')
-def multiplicar( a, b, c = 1, d = 1 ):
-    return a * b * c * d
+def multiplicar( *args, **kwargs ):
+    resul = 1
+    if  args : 
+        for i in args:
+            resul *= i
+
+    if  kwargs : 
+        for v in kwargs.values():
+            resul *= v  
+
+    return resul
 
 lista = [5, 7, 6, 2]
 mapa =  {'a': 3, 'b': 5, 'c' : 4, 'd' : 9}
-print( suma( 2, 6 ) )
-print( multiplicar( 3, 6, 3 ) )
-print( suma( *lista ) )
-print( multiplicar( **mapa ) )
+print( 'Resultado:', suma( 2, 6 ) )
+print( 'Resultado:', multiplicar( 3, 6, 3 ) )
+print( 'Resultado:', suma( *lista ) )
+print( 'Resultado:', multiplicar( **mapa ) )
